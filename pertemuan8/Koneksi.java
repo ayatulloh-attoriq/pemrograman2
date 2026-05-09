791151d837ca57781b098fd1b4dd6503c228f253
ayatulloh-attoriq <attoriqayatulloh12@gmail.com>
2026-05-09 10:27:57 +0700

    Add files via upload

---
+++ pertemuan8/Koneksi.java
@@ -0,0 +1,28 @@
+// NAMA    : AYATULLOH ATTORIQ
+// NIM     : 231011400970
+// KELAS   : 06TPLE016
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://127.0.0.1:3306/dbaplikasigajikaryawan";
+                String user = "root";
+                String password = "123456";
+                
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Koneksi Database Berhasil");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("Koneksi Database Gagal: " + e.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
